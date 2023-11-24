6ce88dd7a9d2cee76c3a98d70a24f41fd282322b
Chiebidolu Chinaemerem <chiboyekene1@gmail.com>
2023-11-24 22:19:17 +0100

    Update utils.ts

--- server/utils/utils.ts
+++ server/utils/utils.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath } from 'node:url';
 export const allowedTypes = new Set(['tables', 'chairs', 'cabinets', 'wardrobes', 'cupboard', 'tv-stands', 'bookshelves', 'beds']);
-console.log(import.meta.url);
-export const galleryURL = !import.meta.dev ?  '.output/server/image-gallery': fileURLToPath(new URL("./image-gallery", import.meta.url).href);
+export const galleryURL = !import.meta.dev ? ( 
+import.meta.preset=="vercel" ? '.vercel/output/functions/__nitro.func/image-gallery':'.output/server/image-gallery'): 
+fileURLToPath(new URL("./image-gallery", import.meta.url).href);
