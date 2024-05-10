cddcbc7ae232a4cc6020553e30ed69a82cc8301a
Chiboy <chiboyekene1@gmail.com>
2024-05-11 00:54:53 +0100

    Test (#15)
    * Other Changes
    
    * Change PortHeader.vue
    
    ---------
    
    Co-authored-by: Chiebidolu Chinaemerem <parkobaofficial@gmail.com>

--- components/PortHeader.vue
+++ components/PortHeader.vue
@@ -10,7 +10,7 @@ function openNav() {
         duration: 300,
         fill: 'both',
         easing: 'ease-in-out'
-    }).onfinish = () => {
+    }).onfinish = () => { 
         isMenuOpen = true;
     }
 }
