5f76c9a6fdc99294cb77643f40a221965391df02
Chiebidolu Chinaemerem <chiboyekene1@gmail.com>
2024-05-27 14:37:53 +0100

    New Changes

--- pages/gallery/index.vue
+++ pages/gallery/index.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
 import { useImage, type UseAsyncStateReturn } from '@vueuse/core';
 import 'swiper/css';
-<<<<<<< HEAD
 import type { WatchStopHandle } from 'vue';
 import type { GalleryData } from '~/types';
-=======
-import type { GalleryData } from '~/types';
-let gallery = ref<GalleryData>({});
->>>>>>> a75ba2571e4dd0ae27aabd334c515ecbb99e1e1d
+
 useHead({
   link: [
     { rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Nunito&display=swap" }
@@ -35,28 +31,13 @@ useHead({
 const { data, error, pending } = await useFetch('/api/gallery', {
   lazy: true,
   server: false,
-<<<<<<< HEAD
+
   deep: false
 });
 
 const isPending = debouncedRef(pending, 2000);
 
 const [galleryParent] = useAutoAnimate({ easing: 'ease-in-out', duration: 500 });
-=======
-});
-const isPending = debouncedRef(pending, 1000);
-
-let galleryWatcher = watch(pending, () => {
-  if (data.value) {
-    gallery.value = data.value.galleryData;
-    data.value = null;
-    galleryWatcher();
-  }
-});
-
-const [galleryParent] = useAutoAnimate({easing: 'ease-in-out', duration: 500});
-onUnmounted(galleryWatcher);
->>>>>>> a75ba2571e4dd0ae27aabd334c515ecbb99e1e1d
 </script>
 
 <template>
@@ -65,20 +46,13 @@ onUnmounted(galleryWatcher);
       <h2>Welcome to our Gallery</h2>
     </div>
     <div v-if="isPending" class="flex justify-center items-center w-full h-full">
-<<<<<<< HEAD
       <MyIcon name="eos-icons:loading" class="text-pb-600 mt-20" size="150" />
     </div>
     <div v-else-if="error"></div>
     <LazyGallery v-else :gallery-data="data!.galleryData" />
-=======
-      <MyIcon name="eos-icons:loading" class="text-pb-600" size="150" />
-    </div>
-    <div v-else-if="error">
-
-    </div>
-    <LazyGallery v-else :gallery-data="gallery" />
->>>>>>> a75ba2571e4dd0ae27aabd334c515ecbb99e1e1d
+    <MyIcon name="eos-icons:loading" class="text-pb-600" size="150" />
   </div>
+
 </template>
 
 <style scoped>
